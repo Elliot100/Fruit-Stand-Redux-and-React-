ba2dc5f2fce1f050427de8cfa3e06c3afd436231
Elly <ellychau10@yahoo.com>
2021-11-13 15:45:04 -0800

    Include new reducer

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store';
+
+window.store = store;
 
 const App = () => (
 	<div>Hello Elliot!</div>
---
+++ frontend/reducer.js
@@ -0,0 +1,18 @@
+const _defaultState = {
+  fruits: []
+};
+
+const reducer = (oldState = _defaultState, action) => {
+  switch(action.type) {
+    case "ADD_FRUIT": 
+      return {
+        fruits: [...oldState.fruits, action.fruit]
+      }
+    case "CLEAR": 
+      return _defaultState;
+    default: 
+      return oldState;
+  }
+}
+
+export default reducer;
