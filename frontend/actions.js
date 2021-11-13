405c8a1a9532ff3b02c94084c3b197cfbf217fea
Elly <ellychau10@yahoo.com>
2021-11-13 15:57:05 -0800

    Add 3 actions creators addApple, addOrange, and clearFruit

---
+++ frontend/actions.js
@@ -0,0 +1,13 @@
+export const addApple = () => ({
+  type: 'ADD_FRUIT',
+  fruit: 'apple'
+});
+
+export const addOrange = () => ({
+  type: 'ADD_FRUIT',
+  fruit: "orange",
+});
+
+export const clearFruit = () =>({
+  type: 'CLEAR'
+})
--- frontend/entry.js
+++ frontend/entry.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
+import { addApple, addOrange, clearFruit } from './actions';
 
+// for testing only
 window.store = store;
+window.addApple = addApple;
+window.addOrange = addOrange;
+window.clearFruit = clearFruit;
 
 const App = () => (
 	<div>Hello Elliot!</div>
