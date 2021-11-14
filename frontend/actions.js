268f8228aa768ec05bcb56afcedd719ddc512c8d
Elly <ellychau10@yahoo.com>
2021-11-13 16:13:32 -0800

    Changed action type strings to constants

--- frontend/actions.js
+++ frontend/actions.js
@@ -1,13 +1,16 @@
+export const ADD_FRUIT = 'ADD_FRUIT';
+export const CLEAR = 'CLEAR';
+
 export const addApple = () => ({
-  type: 'ADD_FRUIT',
+  type: ADD_FRUIT,
   fruit: 'apple'
 });
 
 export const addOrange = () => ({
-  type: 'ADD_FRUIT',
+  type: ADD_FRUIT,
   fruit: "orange",
 });
 
 export const clearFruit = () =>({
-  type: 'CLEAR'
+  type: CLEAR
 })
--- frontend/reducer.js
+++ frontend/reducer.js
@@ -1,14 +1,16 @@
+import { ADD_FRUIT, CLEAR } from "./actions";
+
 const _defaultState = {
   fruits: []
 };
 
 const reducer = (oldState = _defaultState, action) => {
   switch(action.type) {
-    case "ADD_FRUIT": 
+    case ADD_FRUIT: 
       return {
         fruits: [...oldState.fruits, action.fruit]
       }
-    case "CLEAR": 
+    case CLEAR: 
       return _defaultState;
     default: 
       return oldState;
